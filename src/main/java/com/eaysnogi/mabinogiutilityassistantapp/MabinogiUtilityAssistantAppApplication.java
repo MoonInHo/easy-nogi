dee5061ec3dae426fdc32ed9eb4ec9993b317e63
MoonInHo <ansdlsgh55@gmail.com>
2024-09-09 14:40:48 +0900

    first commit

---
+++ src/main/java/com/eaysnogi/mabinogiutilityassistantapp/MabinogiUtilityAssistantAppApplication.java
@@ -0,0 +1,13 @@
+package com.eaysnogi.mabinogiutilityassistantapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MabinogiUtilityAssistantAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MabinogiUtilityAssistantAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eaysnogi/mabinogiutilityassistantapp/MabinogiUtilityAssistantAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eaysnogi.mabinogiutilityassistantapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MabinogiUtilityAssistantAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
